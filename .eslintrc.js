6637f7fae6cc6453aa43241bb9bd61fc24e115c7
alextavella <eu.alextavella@gmail.com>
2021-01-14 15:19:56 -0300

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+
+    'react-hooks/rules-of-hooks': 'off',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/prop-types': 'off',
+
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/comma-spacing': 'off',
+    '@typescript-eslint/return-await': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    'import/export': 'off',
+    'no-useless-constructor': 'off',
+    'no-use-before-define': 'off',
+    'no-extra-boolean-cast': 'off',
+    'prefer-promise-reject-errors': 'off',
+    camelcase: 'off',
+  },
+}
