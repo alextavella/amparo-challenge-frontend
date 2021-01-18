d10ed579ece9dc756294f69221cfa9a0d2eb7fa4
alextavella <eu.alextavella@gmail.com>
2021-01-18 17:43:28 -0300

    improve range of steps on pagination

--- src/presentation/components/pagination/pagination.tsx
+++ src/presentation/components/pagination/pagination.tsx
@@ -5,22 +5,38 @@ import { Container } from './pagination.styles'
 type PaginationProps = {
   page: number
   total: number
+  stepSize?: number
   onClick(page: number): void
 }
 
 const Pagination: React.FC<PaginationProps> = ({
   page: currentPage,
   total,
+  stepSize = 5,
   onClick,
 }) => {
-  const pages = React.useMemo<number[]>(
-    () => Array.from({ length: total }, (_, i) => i + 1),
-    [total],
+  const step = React.useMemo<{ min: number; max: number }>(() => {
+    const middle = Math.round(stepSize - Math.ceil(5 * 0.5))
+
+    let min = Math.max(currentPage - middle, 0)
+    const max = Math.min(Math.ceil(min + stepSize), total)
+    min = Math.max(max - 1 - stepSize, 0)
+
+    return { min, max }
+  }, [currentPage, stepSize, total])
+
+  const numPages = React.useMemo<number[]>(
+    () =>
+      Array.from(
+        { length: Math.min(total, stepSize) },
+        (_, i) => step.min + i + 1,
+      ),
+    [step.min, stepSize, total],
   )
 
   return (
     <Container>
-      {pages.map(page => (
+      {numPages.map(page => (
         <li key={page}>
           <Button disabled={page === currentPage} onClick={() => onClick(page)}>
             {page}
